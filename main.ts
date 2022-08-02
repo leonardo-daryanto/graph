b6f0f6c75908e7ecfb9e53b2fcaaac8506bc3991
leonardo-daryanto <103314397+leonardo-daryanto@users.noreply.github.com>
2022-08-02 14:15:12 +0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
-basic.forever(function () {
-	
-})
+namespace led {
+    /**
+     *  make a graph with starting
+     */
+    //% block
+    //% group="Advanced For Graph"
+    export function graph(value: number, value2: number): void {
+
+        led.plotBarGraph(value, value2)
+    }
+}
